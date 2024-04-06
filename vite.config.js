1690ffea043eeb142959b0e783f046127b203314
Arnob <arnob@gmail.com>
2024-04-06 14:27:49 +0600

    now we can use shadcn components

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import react from "@vitejs/plugin-react"
 
-// https://vitejs.dev/config/
-export default defineConfig({
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+};
