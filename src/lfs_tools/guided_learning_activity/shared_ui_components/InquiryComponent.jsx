47a2653a28de051b7d86c64dd0ef54713c409d6b
Arnob <arnob@gmail.com>
2024-04-06 13:03:32 +0600

    changes i forgot to make in the previous commit. Deleted unnecessary code.

--- src/lfs_tools/guided_learning_activity/shared_ui_components/InquiryComponent.jsx
+++ src/lfs_tools/guided_learning_activity/shared_ui_components/InquiryComponent.jsx
@@ -4,25 +4,12 @@
  * but also used in the author end, to preview the designed inquiry.
  */
 
-import { INQUIRIES } from "../student_end/test_db"
 
-const InquiryComponent = ({ inquiry, enterBranch, breakBranch }) => {
-
-    const branchInquiries = INQUIRIES.filter(inquiry => inquiry.branch == 1)
-
-    const handleBranchEnter = () => {
-        enterBranch(inquiry, branchInquiries)
-    }
-
-    const hangleBreakingBranch = () => {
-        breakBranch()
-    }
+const InquiryComponent = ({ inquiry }) => {
 
     return (
         <>
             <h1>{inquiry.context_text}</h1>
-            <button onClick={handleBranchEnter}>enter branch</button>
-            <button onClick={hangleBreakingBranch}>break branch</button>
         </>
     )
 }
