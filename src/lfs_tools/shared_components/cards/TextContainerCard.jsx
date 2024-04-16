3433bf2091961fbae3186cfc614ec17413ad4624
Arnob <arnob@gmail.com>
2024-04-16 08:27:38 +0600

    revised the styling

--- src/lfs_tools/shared_components/cards/TextContainerCard.jsx
+++ src/lfs_tools/shared_components/cards/TextContainerCard.jsx
@@ -4,7 +4,7 @@ const TextContainerCard = ({ text, label }) => {
     return (
         <>
             <TypographySmall text={label} />
-            <div className="flex flex-col space-y-3 rounded-lg p-5" style={{ backgroundColor: 'var(--card)', color: 'var(--card-foreground)' }}>
+            <div className="flex flex-col space-y-3 rounded-xl p-5" style={{ backgroundColor: 'var(--card)', color: 'var(--card-foreground)' }}>
                 <TypographyP text={text} />
             </div>
         </>
