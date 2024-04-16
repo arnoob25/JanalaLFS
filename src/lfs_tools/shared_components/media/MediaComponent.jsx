af09a7d1fc3dc165a7d750b3803623fa77aee56d
Arnob <arnob@gmail.com>
2024-04-16 08:52:26 +0600

    media component can display sandboxes.

--- src/lfs_tools/shared_components/media/MediaComponent.jsx
+++ src/lfs_tools/shared_components/media/MediaComponent.jsx
@@ -1,8 +1,29 @@
+// MediaComponent.js
+import snakeGame from '@/lfs_tools/guided_learning_activity/test_data/snakeGame';
+import MinimalJsSandbox from './sandboxes/MinimalJsSandbox';
 
-const MediaComponent = () => {
+const MediaComponent = ({ inquiry }) => {
+
+  /**
+   * TODO: fetch the media.
+   * if more than one exists, 
+   *    check the metadata and render appropriate media selectors 
+   *        (tabs/ carousel/ side-by-side)
+   */
+
+  /**
+   * check media type for each type of media and 
+   *    select appropriate component to display them
+   */
+
+
   return (
-    <div>MediaComponent</div>
-  )
-}
+    <div className="border-2 border-var(--border) rounded-xl">
+      <MinimalJsSandbox
+        simCode={snakeGame}
+      />
+    </div>
+  );
+};
 
-export default MediaComponent
+export default MediaComponent;
