3eeccb208c78bbf5b05f4810c64ac1793cab925e
Arnob <arnob@gmail.com>
2024-05-01 16:44:49 +0600

    enabled conditionally exiting or repeating branches.

--- src/lfs_tools/guided_learning_activity/student_end/helpers/hooks/useProgressionHook.jsx
+++ src/lfs_tools/guided_learning_activity/student_end/helpers/hooks/useProgressionHook.jsx
@@ -1,7 +1,6 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 import { INQUIRIES } from "@/assets/test_data/test_db"
 import { flushSync } from "react-dom"
-
 
 /**
  * TODO: convert this hook into a generalized hook 
@@ -14,15 +13,17 @@ export default function useProgression(allMainInquiries, manageGlaEnd) {
 
     // states for enabling branching progression
     const [shouldEnterBranch, setShouldEnterBranch] = useState(false)
-    const [selectedBranch, setSelectedBranch] = useState(undefined) // current branch
-    const [branchParentInquiry, setBranchParentInquiry] = useState(undefined) // inquiry that originated the selected branch
+    const [selectedBranch, setSelectedBranch] = useState(null) // current branch
+    const [branchParentInquiry, setBranchParentInquiry] = useState(null) // inquiry that originated the selected branch
     const [allBranchInquiries, setAllBranchInquiries] = useState([]) // stores all the inquiries in the active branch
+    const isLastBranchInquiry = useRef(false)
     const [shouldExitBranch, setShouldExitBranch] = useState(false) // dictates if we should return to the main set of inquiries
 
 
     // given a list of inquiries, can set the next inquiry as the selected inquiry. Returns false if next inquiry doesn't exist.
     const selectNextInquiry = (listOfInquiries, indexOfCurrentInquiry = null) => {
-        if (!indexOfCurrentInquiry) {
+
+        if (indexOfCurrentInquiry === null) {
             indexOfCurrentInquiry = listOfInquiries.indexOf(selectedInquiry)
         }
 
@@ -30,56 +31,52 @@ export default function useProgression(allMainInquiries, manageGlaEnd) {
 
         if (nextInquiry) {
             setSelectedInquiry(nextInquiry)
+
+            // If we're in a branch, check if this is the last inquiry
+            if (selectedBranch) checkIfLastBranchInquiry(nextInquiry)
+
             return true
         }
 
         else { return false }
     }
 
-    const manageProgression = () => {
+    const manageProgression = (forceExitBranch = false) => {
         // branching progression
         if (selectedBranch) {
-            if (shouldExitBranch) {
-                const isLastInquiryInTheBranch = allBranchInquiries.indexOf(selectedInquiry) == allBranchInquiries.length - 1
-
+            // optional argument allows bypassing setting an extra state
+            if ((isLastBranchInquiry.current && shouldExitBranch) || forceExitBranch) {
                 // exits the branch, and resumes the main set of inquiries
-                if (isLastInquiryInTheBranch) {
-                    const indexOfCurrentInquiry = allMainInquiries.indexOf(branchParentInquiry)
-                    const hasNextInquiry = selectNextInquiry(allMainInquiries, indexOfCurrentInquiry)
+                const indexOfCurrentInquiry = allMainInquiries.indexOf(branchParentInquiry)
+                const hasNextInquiry = selectNextInquiry(allMainInquiries, indexOfCurrentInquiry)
 
-                    if (!hasNextInquiry) {
-                        manageGlaEnd()
-                    }
-                    resetBranch()
-                }
+                if (!hasNextInquiry) manageGlaEnd()
+
+                resetBranch()
             }
             // continues with the next inquiry in the branch
             else {
                 const hasNextInquiry = selectNextInquiry(allBranchInquiries)
 
-                if (!hasNextInquiry) {
-                    manageBranchRepetition()
-                }
+                if (!hasNextInquiry) manageBranchRepetition()
             }
         }
         // linear progression
         else {
             const hasNextInquiry = selectNextInquiry(allMainInquiries)
 
-            if (!hasNextInquiry) {
-                manageGlaEnd()
-            }
+            if (!hasNextInquiry) manageGlaEnd()
         }
     }
 
     // optional argument forces progression in special cases. e.g. text responses
-    const handleProgressionRequest = (shouldProgress = false) => {
-        if (shouldAllowProgression || shouldProgress) {
+    const handleProgressionRequest = (forceProgress = false, forceExitBranch = false) => {
+        if (shouldAllowProgression || forceProgress) {
             if (shouldEnterBranch) {
                 enterBranch()
             }
             else {
-                manageProgression()
+                manageProgression(forceExitBranch)
             }
         }
         else {
@@ -90,25 +87,39 @@ export default function useProgression(allMainInquiries, manageGlaEnd) {
     }
 
     // for enabling branching progression
-
-    // TODO: replace this with a query 
+    /**
+     *  TODO: replace this with a query
+     *  this wouldn't work inside the hook,since the hook is generalized
+     *  we won't have query functions. Or maybe we can pass the query functions. 
+     *  in that case, we can also query the corresponding branch from the selected choice.
+     */
     const getBranchInquiries = selectedBranch => {
         selectedBranch
         return INQUIRIES.filter(inquiry => inquiry.branch == selectedBranch.id) || null
     }
 
-    // TODO: dynamically set the selected branch
     const handleBranchInitialization = selectedBranch => {
         // only initialize when a branch has been selected
         if (selectedBranch) {
             const filteredInquiries = getBranchInquiries(selectedBranch)
 
             if (filteredInquiries) {
-                flushSync(() => {
-                    setBranchParentInquiry(selectedInquiry)
-                    setSelectedBranch(selectedBranch)
-                    setShouldEnterBranch(true)
-                    setAllBranchInquiries(filteredInquiries)
+                /**
+                 * schedules the state updates after the current render cycle ends.
+                 * this prevents warnings in the console, and potential bugs and infinite loops.
+                 */
+                queueMicrotask(() => {
+                    /**
+                     * immediately updates the states
+                     * otherwise, they'd get batched with other state updates, 
+                     * at the next trigger.
+                     */
+                    flushSync(() => {
+                        setBranchParentInquiry(selectedInquiry)
+                        setSelectedBranch(selectedBranch)
+                        setShouldEnterBranch(true)
+                        setAllBranchInquiries(filteredInquiries)
+                    })
                 })
             }
         }
@@ -116,34 +127,42 @@ export default function useProgression(allMainInquiries, manageGlaEnd) {
     }
 
     const enterBranch = () => {
-        flushSync(() => {
-            setSelectedInquiry(allBranchInquiries[0])
-            setShouldEnterBranch(false)
-        })
+        setSelectedInquiry(allBranchInquiries[0])
+        setShouldEnterBranch(false)
+    }
+
+    const checkIfLastBranchInquiry = (branchInquiry) => {
+        const isLastInquiry = allBranchInquiries.indexOf(branchInquiry) == allBranchInquiries.length - 1;
+
+        isLastBranchInquiry.current = isLastInquiry
     }
 
     const manageBranchRepetition = () => {
+        isLastBranchInquiry.current = false
         setSelectedInquiry(branchParentInquiry)
     }
 
     const resetBranch = () => {
+        isLastBranchInquiry.current = false
         setShouldAllowProgression(false)
         setShouldEnterBranch(false)
-        selectedBranch('')
-        setBranchParentInquiry('')
+        setSelectedBranch(null)
+        setBranchParentInquiry(null)
         setAllBranchInquiries([])
         setShouldExitBranch(false)
     }
 
-    return [
+    return {
         selectedInquiry,
         handleProgressionRequest,
         handleBranchInitialization,
+        isLastBranchInquiry,
+        setShouldExitBranch,
         /**
-         * the following are included in the hook's api, but can be skipped 
-         * by forcing progression with the optional argument in the handleProgresionRequest
+         * the following can be skipped by forcing progression 
+         * with the optional argument (true) in the handleProgresionRequest
          */
         shouldAllowProgression,
         setShouldAllowProgression,
-    ]
+    }
 }
